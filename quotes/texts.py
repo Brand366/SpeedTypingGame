266a26dfb706a39f1fe86ae4be17b71c756bab9e
Brand366 <brandon.vonhoff@yahoo.com>
2021-07-14 18:45:05 +1000

    Slight redesign for difficulty function. Will need to create more functions for other difficulties

--- quotes/texts.py
+++ quotes/texts.py
@@ -1,11 +1,29 @@
 import gzip
 import json
+import random
 
-def read_text(examples.json.gz):
-    #Dictionary that will contain text entries
-    dictionary = {}
+#Selects text body relative to text length and then store that data to easy_text, medium_text or hard_text depending on length of text
+def text_selector():
     with gzip.open('examples.json.gz', 'r') as f:
         data = json.load(f)
-    return data
+        #sorted_data = sorted(data, key=lambda x: [2])
+        easy_text = ""
+        med_text = ""
+        hard_text = ""
+        random_text = ""
+
+        while not (60 < len(random_text) <= 100):
+            random_entry = random.choice(data)
+            random_text = random_entry[2]
+
+            return easy_text.join(random_text)
+            
+            if len(random_text) <= 200 and len(random_text) > 101:
+                return medium_text.join(random_text)
+                
+            else:
+                return hard_text.join(random_text)
 
 print(read_text())
+
+
