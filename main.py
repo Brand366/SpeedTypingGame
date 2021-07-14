dd4c26a5739ef695071957b1d4f678dfcaa68901
Brand366 <brandon.vonhoff@yahoo.com>
2021-07-14 18:47:50 +1000

    Completed basic validator function for number input by user. Began start_test function, will need to complete and test. Also, improved effiency of print_center function.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import sys
-import os
+import shutil
 import time
 import random
 
 #Center output through print
 def print_center(row):
-    width = os.get_terminal_size().columns
-    print(row.center(width))
+    print(row.center(shutil.get_terminal_size().columns))
 
 #Application introduction
 def title_screen():
@@ -23,44 +22,36 @@ def title_screen():
     print_center("By Brandon Vonhoff\n\n")
     print_center("This application will test your typing ability, so make sure you're ready!\n\n")
     print_center("First, please select the desired level of difficulty:\n\n")
-    print_center("Press '1' for [Easy], '2' for [Medium], '3' for [Hard]\n\n")
-    print_center("Once the desired difficulty is selected, press the 'enter' key to begin. Have fun!")
-
-#Number validator for numbered options
-def num_validator():
+    
 
 
-
-
-
-
-
-
-
-
-
+#validator for user options
+def validator(prompt):
+    while True:
+        try:
+            user_input = int(input(prompt))
+            if user_input in [1, 2, 3]:
+                break
+            else:
+                print_center("Sorry, please enter a valid number.\n")
+        except ValueError:
+            print_center("Sorry, please enter a valid number.\n")
 
 
 
+def start_test():
 
+    difficulties = {
+        1 : easy_text()
+        2 : med_text()
+        3 : hard_text()
+    }
 
+    difficulty = valid_input("Press '1' for [Easy], '2' for [Medium], '3' for [Hard]\n\n")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if difficulty in difficulties:
+        print_center("Awesome! Press the 'enter' key to begin. Have fun!\n")
 
 if __name__ == '__main__':
     title_screen()
+    validator()
